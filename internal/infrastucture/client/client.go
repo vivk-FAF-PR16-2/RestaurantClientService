0feca174df935ffde3bc25d26f6a887c2b8a1ceb
Volcov Oleg <bojikch15@gmail.com>
2021-10-28 11:01:02 +0300

    Get `menu` from food ordering server and implement it in `order` sending behaviour.

--- internal/infrastucture/client/client.go
+++ internal/infrastucture/client/client.go
@@ -45,9 +45,15 @@ func (c *client) Update() {
 }
 
 func (c *client) order() {
+	addr := viper.GetString("food_ordering_addr")
+	addrMenu := fmt.Sprintf("http://%s/menu", addr)
+	responseMenu := sendrequest.Get(addrMenu, nil)
+
 	var menu dto.MenuData
-	// TODO: Get menu from food ordering
-	menu = dto.MenuData{}
+	err := json.NewDecoder(responseMenu.Body).Decode(&menu)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
 
 	clientData := GenerateOrder(c.id, menu)
 	jsonData, err := json.Marshal(clientData)
@@ -55,11 +61,11 @@ func (c *client) order() {
 		log.Fatalf("error: %v", err)
 	}
 
-	addr := fmt.Sprintf("http://%s/order", viper.GetString("food_ordering_addr"))
-	response := sendrequest.Post(addr, jsonData)
+	addrOrder := fmt.Sprintf("http://%s/order", addr)
+	responseOrder := sendrequest.Post(addrOrder, jsonData)
 
 	var responseData dto.ClientOutData
-	err = json.NewDecoder(response.Body).Decode(&responseData)
+	err = json.NewDecoder(responseOrder.Body).Decode(&responseData)
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
